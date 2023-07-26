10fb1e4bde3d9a3d6dd61c28709a722f93ef69fe
Komsomol <Komsomol@users.noreply.github.com>
2023-07-26 21:07:15 +0100

    Port for Azure web app

--- app.js
+++ app.js
@@ -111,6 +111,9 @@ app.post("/submit", (req, res) => {
   });
 });
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
 });
